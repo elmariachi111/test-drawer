24b1c0a82d29dc11dd25ee93a6d1c57fefbdbe64
Stefan <stadolf@gmail.com>
2022-07-10 21:47:40 +0200

    chakra drawer sample on next 12.2
    Signed-off-by: Stefan <stadolf@gmail.com>

---
+++ components/Sidebar.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+export function Sidebar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button colorScheme="teal" onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
